d983b709040cb28f1aa6cc63f8b59a1c4bd4d83a
Derek Stapleton <derek.stapleton21@gmail.com>
2020-03-19 19:13:50 -0400

    javascript shit

---
+++ script.js
@@ -0,0 +1,12 @@
+var firstName = "Derek"
+console.log(firstName)
+var lastName = "Stapleton"
+console.log(lastName)
+var age = 28;
+console.log(age)
+var likesTravel = true;
+console.log(likesTravel)
+
+var movieArray = ["Saving Private Ryan", "Field of Dream",]
+console.log(movieArray[0])
+console.log(movieArray[1])
